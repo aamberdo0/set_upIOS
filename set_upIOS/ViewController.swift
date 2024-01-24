5710484946b7f7dc48ac7c7aff8fe7d772dadff1
amber Do <anhdosheridan@gmail.com>
2024-01-23 21:43:50 -0500

    Xcode project updated

--- set_upIOS/ViewController.swift
+++ set_upIOS/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var mySwitch : UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+ 
+        @IBAction func changeBackgroundColor(_ sender: UIButton){
+            let randomColor = changeColor()
+            view.backgroundColor = randomColor;
+           
+        }
+    
 
+    func changeColor()->UIColor{
+        let red = CGFloat.random(in:0...1)
+        let green = CGFloat.random(in:0...1)
+        let blue = CGFloat.random(in:0...1)
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:0.5);
+    }
+    @IBAction func changeSwitch(_ sender: UISwitch) {
+        if sender.isOn{
+            view.backgroundColor = .red;
+        }else{
+            view.backgroundColor = .yellow;
+        }
+    }
 }
 
